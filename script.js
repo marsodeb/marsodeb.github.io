10b9bbdab5896eb3e8c6b00e393fc79698ea7fef
marsodeb <marceaud08@icloud.com>
2023-11-04 14:08:25 +0100

    V1 de Mon Portfolio

---
+++ script.js
@@ -0,0 +1,36 @@
+const target = document.getElementById("target");
+let array = ["Hey !✌️", "I'm...", "Student✍", "Developer Web⏳"];
+let wordIndex = 0;
+let letterIndex = 0;
+
+const createLetter = () => {
+  const letter = document.createElement("span");
+  target.appendChild(letter);
+
+  letter.textContent = array[wordIndex][letterIndex];
+
+  setTimeout(() => {
+    letter.remove();
+  }, 1300);
+};
+
+const loop = () => {
+  setTimeout(() => {
+    if (wordIndex >= array.length) {
+      wordIndex = 0;
+      letterIndex = 0;
+      loop();
+    } else if (letterIndex < array[wordIndex].length) {
+      createLetter();
+      letterIndex++;
+      loop();
+    } else {
+      wordIndex++;
+      letterIndex = 0;
+      setTimeout(() => {
+        loop();
+      }, 1300);
+    }
+  }, 60);
+};
+loop();
